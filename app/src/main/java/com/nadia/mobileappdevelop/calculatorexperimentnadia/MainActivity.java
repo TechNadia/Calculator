60ef982e050a170ad791f31217e90030f09b584e
Nadia Akter <tech.nadia@gmail.com>
2015-04-26 15:57:04 +0600

    added square button

--- app/src/main/java/com/nadia/mobileappdevelop/calculatorexperimentnadia/MainActivity.java
+++ app/src/main/java/com/nadia/mobileappdevelop/calculatorexperimentnadia/MainActivity.java
@@ -2,6 +2,7 @@ package com.nadia.mobileappdevelop.calculatorexperimentnadia;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -152,6 +153,12 @@ public class MainActivity extends ActionBarActivity{
         str = str + "/";
         etFirstNumber.getText().clear();
         etFirstNumber.setText(str);
+    }
+
+    public void square(View v){
+        Float number = Float.valueOf(str);
+        tvResult.setText(String.valueOf(number * number));
+
     }
     public void equal(View view){
 
